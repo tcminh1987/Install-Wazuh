1b40231a7d106d7d85d8b74669749cbefd282757
AlumiNoobie <aluminoobie@users.noreply.github.com>
2019-02-24 00:17:30 -0500

    Update Install-Wazuh.ps1

--- Install-Wazuh.ps1
+++ Install-Wazuh.ps1
@@ -58,7 +58,7 @@ Write-Host "Configuration File Retrieved..."
 
 Write-Host "Installing Wazuh..."
 
-.\wazuh-agent-3.8.2-1.msi /q ADDRESS="172.20.3.35" AUTHD_SERVER="172.20.3.35" PASSWORD="ossec" AGENT_NAME="$agentname" /l*v installer.log
+.\wazuh-agent-3.8.2-1.msi /q ADDRESS="172.18.42.125" AUTHD_SERVER="172.18.42.125" PASSWORD="ossec" AGENT_NAME="$agentname" /l*v installer.log
 
 Copy-Item ossec.conf -Destination 'C:\Program Files (x86)\ossec-agent\'
 
